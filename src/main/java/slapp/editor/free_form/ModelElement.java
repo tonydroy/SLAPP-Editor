b03803aa16daaac7e854de20f1cf3f3f41cedb1e
tonyd <tonydroy@gmail.com>
2024-08-21 14:02:15 -0700

    Copyright notices (2).

--- src/main/java/slapp/editor/free_form/ModelElement.java
+++ src/main/java/slapp/editor/free_form/ModelElement.java
@@ -1,4 +1,19 @@
-package slapp.editor.free_form;
+/*
+Copyright (c) 2024 Tony Roy
+
+This file is part of the Symbolic Logic APPlication (SLAPP).
+
+SLAPP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+SLAPP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with SLAPP.  If not, see
+<https://www.gnu.org/licenses/>.
+ */
+
+ackage slapp.editor.free_form;
 
 import slapp.editor.main_window.Exercise;
 import slapp.editor.main_window.ExerciseModel;
